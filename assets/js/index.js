8621a8671183858f752c55db8c7648990b15a919
seikaku01 <seikaku.jh@gmail.com>
2025-03-05 22:32:56 +0900

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,10 @@ function toggleMenu() {
     const navLinks = document.querySelector('nav ul.nav-links');
     navLinks.classList.toggle('show');
 }
+
+document.getElementById('moreInfoBtn').addEventListener('click', function() {
+    document.querySelectorAll('.info-item.hidden').forEach(item => {
+        item.classList.remove('hidden');
+    });
+    this.style.display = 'none';  // ボタンを非表示にする
+});
